f62951552f45b8dde2063b0e87dded5177ed6c2f
orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-04-28 13:42:30 -0300

    feat: implementa sumario e header

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Transations } from "./pages/Transactions"
 
 export function App() {
 
   return (
     <>
-     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
-      <h1>Hello World</h1>
-     </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Transations/>
+      </ThemeProvider>
     </>
   )
 }
