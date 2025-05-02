f12d8667d1c922f38b6bffaff1c70bd3c3335cb7
Orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-05-02 16:55:00 -0300

    chore: melhora interação dos botões

--- dt-money/src/components/NewTransactionModal/Index.tsx
+++ dt-money/src/components/NewTransactionModal/Index.tsx
@@ -1,6 +1,6 @@
 import * as  Dialog from "@radix-ui/react-dialog"
-import { CloseButton, Content, Overlay } from "./styles"
-import { X } from "phosphor-react"
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles"
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react"
 
 
 export function NewTransactionModal() {
@@ -29,7 +29,17 @@ export function NewTransactionModal() {
                         placeholder="Categoria"
                         required
                     />
+                    <TransactionType>
+                        <TransactionTypeButton variant="income">
+                            <ArrowCircleUp size={24}/>
+                            Entrada
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant="outcome">
+                            <ArrowCircleDown size={24}/>
+                            Saída
+                        </TransactionTypeButton>
 
+                    </TransactionType>
                     <button type="submit">Cadastrar</button>
                 </form>
                 <CloseButton>
